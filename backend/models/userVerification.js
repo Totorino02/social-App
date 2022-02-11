3a6bffc90bc4801c9a59838ac7c997aaa300509e
totorino <antoinehounsi3@gmail.com>
2022-02-11 22:38:48 +0000

    userVerification

---
+++ backend/models/userVerification.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const userVerificationSchema = mongoose.Schema({
+    userId: {type: mongoose.Types.ObjectId, ref: "User"},
+    uniqueString:{type: String, required:true},
+    createdAt: {type: Date, required: true},
+    expiredAt: {type: Date, required: true}
+});
+
+module.exports = mongoose.model("UserVerification", userVerificationSchema);
