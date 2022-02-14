aef392dabf467c9f5af8f3e3da153214480b1f1a
totorino <antoinehounsi3@gmail.com>
2022-02-14 02:58:19 +0000

    userModel Update

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,27 +1,27 @@
 const mongoose = require("mongoose");
+const uniqueValidator = require('mongoose-unique-validator');
 
 const userSchema = mongoose.Schema({
-    name:{type:String, required: true},
+    name: {type:String, required: true},
     username:{type:String, required: true, unique:true},
     email: {type:String, required: false},
     password: {type:String, required:true},
-    sexe: {type:Boolean, required:true},
     phoneNumber: {type:Number, required:false},
     dateOfBirth: {type:Date, required:true},
     isAdmin: {type:Boolean, default:false},
     isVerified: {type:Boolean, default:false},
-    profileImage:{type:String, required: true},
+    profileImage:{type:String, required: false},
     profileInfo:{type: String, required: false},
     followers:{
-        type: [{userId: mongoose.Types.ObjectId, ref:"User"}],
+        type: [{userId: mongoose.Types.ObjectId}],
         default: []
     },
     followings:{
-        type: [{userId: mongoose.Types.ObjectId, ref:"User"}],
+        type: [{userId: mongoose.Types.ObjectId}],
         default: []
     }
-
-
 });
+
+userSchema.plugin(uniqueValidator);
 
 module.exports = mongoose.model("User", userSchema);
