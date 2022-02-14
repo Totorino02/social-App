f912a1f8ab8f6559408466e31322f271a84db2a9
totorino <antoinehounsi3@gmail.com>
2022-02-14 02:58:52 +0000

    userVerificationModel Update

--- backend/models/userVerification.js
+++ backend/models/userVerification.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
+const User = require("./user");
 
 const userVerificationSchema = mongoose.Schema({
-    userId: {type: mongoose.Types.ObjectId, ref: "User"},
+    userId: {type: mongoose.Types.ObjectId, ref: "User", required:true},
     uniqueString:{type: String, required:true},
     createdAt: {type: Date, required: true},
     expiredAt: {type: Date, required: true}
