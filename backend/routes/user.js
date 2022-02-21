bb8c2520851540940b9cddef727399546cb5cdc2
totorino <antoinehounsi3@gmail.com>
2022-02-21 03:15:49 +0000

    +100% change_profile

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,12 +1,16 @@
 const { register, verifyCount, login, authVerification, changProfile } = require("../controllers/userController");
 const router = require("express").Router();
+const multer = require('multer');
+const { profileStorage } = require("../utils/saveImage");
+
+const upload = multer({storage: profileStorage}).single("profile");
 
 
 router.post("/register", register);
 
 router.post("/login", login);
 
-router.post("/profile", authVerification, changProfile);
+router.post("/profile", authVerification, upload, changProfile);
 
 router.get("/verification/:id/:hashedId", verifyCount);
 
